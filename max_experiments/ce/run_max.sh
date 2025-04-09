5adac271b4c18dcc15dba9ba4798525111067a7c
Max Van Gastel <Max.vangastel@student.uhasselt.be>
2025-04-09 10:41:08 +0200

    Give up for now on 11b

--- max_experiments/ce/run_max.sh
+++ max_experiments/ce/run_max.sh
@@ -4,7 +4,7 @@ root_dir="../../"
 parquet_data="$root_dir/benchmarks/ce/parquet-zstd"
 out_folder="./output" # non-existing output folder
 timings="timings.csv"
-plan="./query_plans/11e.json"
+plan="./query_plans/11b.json"
 repetitions=1
 
 # make timings file
